a7d2593a580d3f0a7aaa7a9cbc9bc0f2e2f76943
nav <nav@2bd074aa-e4fc-11de-ab96-fd2e03e142b3>
2009-12-09 21:43:23 +0000

    license and comments.

--- appengine/stats.py
+++ appengine/stats.py
@@ -1,3 +1,17 @@
+"""
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+"""
+
 import datetime
 import logging
 import os
@@ -11,6 +25,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 
 class SiteTime(db.Model):
+  """Represents amount of time spent on a site per day."""
   user = db.UserProperty(required=True)
   site = db.StringProperty(required=True)
   seconds = db.FloatProperty(required=True)
@@ -18,26 +33,29 @@ class SiteTime(db.Model):
 
 
 class UserInfo(db.Model):
+  """Information about a particular user."""
   user = db.UserProperty(required=True)
   last_update = db.DateTimeProperty()
 
 
 class View(webapp.RequestHandler):
+  """Displays a web page summary for today's data."""
 
   def get(self):
-    site_times = SiteTime.gql('WHERE user = :1 AND day = :2 ORDER BY seconds DESC',
-                              users.get_current_user(),
+    user = users.get_current_user()
+    site_times = SiteTime.gql('WHERE user = :1 AND day = :2 '
+                              'ORDER BY seconds DESC',
+                              user,
                               datetime.datetime.now().date())
 
-    user_info = UserInfo.gql('WHERE user = :1',
-                             users.get_current_user()).get()
+    user_info = UserInfo.gql('WHERE user = :1', user).get()
     if user_info:
       last_update = user_info.last_update
     else:
       last_update = None
 
     template_values = {
-        'user': users.get_current_user(),
+        'user': user,
         'last_update': last_update,
         'site_times': [{'site': x.site,
                         'minutes': x.seconds/float(60)} for x in site_times]
@@ -48,6 +66,7 @@ class View(webapp.RequestHandler):
 
 
 class Update(webapp.RequestHandler):
+  """Handles update requests from chrome extension."""
 
   def get(self):
     self.error(501)  # Not implemented.
@@ -55,15 +74,16 @@ class Update(webapp.RequestHandler):
   def post(self):
     if not self.request.get('sites'):
       self.error(400)  # Bad request.
+    user = users.get_current_user()
     sites = simplejson.loads(self.request.get('sites'))
 
     updates = []
     day = datetime.datetime.now().date()
     for (site, seconds) in sites.iteritems():
       site_time = SiteTime.gql('WHERE user = :1 AND site = :2 AND day = :3',
-                               users.get_current_user(), site, day).get()
+                               user, site, day).get()
       if not site_time:
-        site_time = SiteTime(user=users.get_current_user(),
+        site_time = SiteTime(user=user,
                              seconds=0.0,
                              site=site,
                              day=day)
@@ -74,15 +94,16 @@ class Update(webapp.RequestHandler):
     except db.TransactionFailedError:
       self.error(500)  # Internal server error.
 
-    user_info = UserInfo.gql('WHERE user = :1', users.get_current_user()).get()
+    user_info = UserInfo.gql('WHERE user = :1', user).get()
     if not user_info:
-      user_info = UserInfo(user=users.get_current_user())
+      user_info = UserInfo(user=user)
     user_info.last_update=datetime.datetime.now()
     user_info.put()
     self.response.set_status(200)
 
 
 class Clear(webapp.RequestHandler):
+  """Clears all stats data for the logged in user."""
 
   def get(self):
     data = SiteTime.gql('WHERE user = :1', users.get_current_user())
